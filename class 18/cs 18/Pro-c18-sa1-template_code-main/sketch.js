fbf41533bec5c8a4c8b6d803b676aab1550a8ef7
rishabhsolanki2008 <97528707+rishabhsolanki2008@users.noreply.github.com>
2022-04-05 10:09:47 +0530

    Add files via upload

---
+++ class 18/cs 18/Pro-c18-sa1-template_code-main/sketch.js
@@ -0,0 +1,14 @@
+var student_1;
+function setup() {
+  createCanvas(400,400);
+student_1 = new Student("Rishabh",14,8)
+student_1.dispaly();
+}
+
+
+
+function draw() 
+{
+  background(30);
+  
+}
---
+++ class 18/cs 18/Pro-c18-sa1-template_code-main/student.js
@@ -0,0 +1,17 @@
+
+class Student
+{
+  constructor(name,age,grade) {
+   this.age = age;
+   this.name = name;
+   this.grade = grade;   
+  } 
+  dispaly() {
+console.log(this.name);
+console.log(this.age);
+console.log(this.grade);
+  }
+}
+
+
+
